323e18492d56a835cfcb2a7b056e97cce15106f6
Omar Rico <83613313+omar-rm@users.noreply.github.com>
2025-02-12 16:43:34 +0000

    new section created

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,6 +6,7 @@ export const Banner = () => {
   return (
     <section className="banner" id="home">
       <Container>
+        <h1>Hello</h1>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={6}>
             <span className="tagline">Junior Software Developer</span>
